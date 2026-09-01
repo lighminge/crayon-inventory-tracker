90baca594b30c51343ae911ab213b60cdf19d1bd
lighminge <lighminge@gmail.com>
2026-09-01 23:11:50 +0800

    feat(firebase): enable app check with recaptcha v3

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAdcWfaFSobqOaI29ZeT2egbqwXUpDRs2g",
@@ -14,6 +15,16 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize App Check
+if (typeof window !== 'undefined') {
+  // 設定在開發環境可以透過 debug token 繞過限制 (選擇性)
+  // self.FIREBASE_APPCHECK_DEBUG_TOKEN = true;
+  initializeAppCheck(app, {
+    provider: new ReCaptchaV3Provider('6LdsdKMtAAAAAAdj6iEMvolYa19W8FuZxE9KNFoe'),
+    isTokenAutoRefreshEnabled: true
+  });
+}
 
 // Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
